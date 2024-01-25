382c05aa2202b4a532e937261073199693935de3
soooxmin <113075916+soooxmin@users.noreply.github.com>
2024-01-25 09:31:59 +0900

    투두리스트 1회차

---
+++ todolist/script.js
@@ -0,0 +1,40 @@
+
+
+
+
+let input = document.getElementById('input');
+let btn = document.getElementById('btn');
+let todos = document.getElementById('todos');
+
+// btn.addEventListener('click', function () {
+//     var list = document.createElement('li');
+
+//     if(!input.value){
+//         alert('nope');
+//     }else{
+//         list.innerText = input.value;
+//         todos.appendChild(list);
+//         input.value = ""
+//     }
+    
+//     list.addEventListener('click',function () {
+//         list.style.textDecoration = "line-through";
+//     })
+//     list.addEventListener('dblclick',function () {  
+//         todos.removeChild(list);
+//     })
+// })
+
+
+const add = function () {
+    if
+}
+
+
+
+
+
+
+
+
+
